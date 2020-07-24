eddc418cdc9e31a0673dd5eeac28f335d7912a02
Sameera Appana <appanasa@Admins-MBP.lan1>
2020-07-24 18:57:26 -0400

    final commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :received_kudos, class_name: "Kudo", foreign_key: :recipient_id
-  has_many :sent_kudos, class_name: "Kudo", foreign_key: :sender_id do
+  has_many :received_kudos, class_name: "Kudo", foreign_key: :recipient_id, dependent: :destroy
+  has_many :sent_kudos, class_name: "Kudo", dependent: :destroy, foreign_key: :sender_id do
     def this_week
       where(:created_at => (Time.zone.now.beginning_of_week..Time.zone.now))
     end
